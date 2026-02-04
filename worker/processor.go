664a9ff7c681636797917aeffc51a99a1b0d93fa
latronico96 <35084047+latronico96@users.noreply.github.com>
2026-02-04 04:33:38 -0300

    Actualizar processor.go
    el procesador no devolvía el estado de temrinado

--- worker/processor.go
+++ worker/processor.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 )
 
-func ProcessJob(job *Job, client *BackendClient) error {
-	switch job.Type {
-	case "VIDEO_CLIP":
-		return ProcessVideoClip(job, client)
-	default:
-		return fmt.Errorf("unknown job type: %s", job.Type)
-	}
+func ProcessJob(job *Job, client *BackendClient) (map[string]any, error) {
+    switch job.Type {
+    case "VIDEO_CLIP":
+        return ProcessVideoClip(job, client)
+    default:
+        return nil, fmt.Errorf("unknown job type: %s", job.Type)
+    }
 }
