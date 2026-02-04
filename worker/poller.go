f06500eb1ed2d37c694094481e7d1d8403f4b835
latronico96 <35084047+latronico96@users.noreply.github.com>
2026-02-04 04:20:08 -0300

    Actualizar poller.go
    borrar bugs

--- worker/poller.go
+++ worker/poller.go
@@ -6,44 +6,42 @@ import (
 )
 
 func StartPolling(client *BackendClient, interval int, workerID string) {
-	log.Println("[WORKER] Polling started")
+    log.Println("[WORKER] Polling started")
 
-	for {
-		job, err := client.FetchNextJob()
-		if err != nil {
-			log.Println("[WORKER] error fetching job:", err)
-			time.Sleep(time.Duration(interval) * time.Second)
-			continue
-		}
+    for {
+        job, err := client.FetchNextJob()
+        if err != nil {
+            log.Println("[WORKER] error fetching job:", err)
+            time.Sleep(time.Duration(interval) * time.Second)
+            continue
+        }
 
-		if job == nil {
-			log.Println("[WORKER] no jobs, sleeping...")
-			time.Sleep(time.Duration(interval) * time.Second * 3)
-			continue
-		}
+        if job == nil {
+            log.Println("[WORKER] no jobs, sleeping...")
+            time.Sleep(time.Duration(interval) * time.Second * 3)
+            continue
+        }
 
-		log.Printf("[WORKER] Job received: id=%s type=%s\n", job.ID, job.Type)
+        log.Printf("[WORKER] Job received: id=%s type=%s\n", job.ID, job.Type)
 
-		stopHB := make(chan struct{})
-		go StartHeartbeat(client, job.ID, workerID, stopHB)
-n
-   err := func() error {
-       defer close(stopHB)
-       return ProcessJob(job, client)
-   }()
+        stopHB := make(chan struct{})
+        go StartHeartbeat(client, job.ID, workerID, stopHB)
 
-		close(stopHB)
+        err = func() error {
+            defer close(stopHB)
+            return ProcessJob(job, client)
+        }()
 
-		if err != nil {
-			client.FailJob(job.ID, err)
-		} else {
-			client.CompleteJob(
-				job.ID,
-				map[string]any{
-					"message": "processed successfully",
-				},
-				workerID,
-			)
-		}
-	}
+        if err != nil {
+            client.FailJob(job.ID, err)
+        } else {
+            client.CompleteJob(
+                job.ID,
+                map[string]any{
+                    "message": "processed successfully",
+                },
+                workerID,
+            )
+        }
+    }
 }
