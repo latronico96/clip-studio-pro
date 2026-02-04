f390b8101028d949c919cae513c23877b0e6a21b
latronico96 <35084047+latronico96@users.noreply.github.com>
2026-02-04 16:39:09 -0300

    Refactor polling and video processing functions to improve error handling and return types

--- worker/poller.go
+++ worker/poller.go
@@ -6,36 +6,36 @@ import (
 )
 
 func StartPolling(client *BackendClient, interval int, workerID string) {
-    log.Println("[WORKER] Polling started")
+	log.Println("[WORKER] Polling started")
 
-    for {
-        job, err := client.FetchNextJob()
-        if err != nil {
-            log.Println("[WORKER] error fetching job:", err)
-            time.Sleep(time.Duration(interval) * time.Second)
-            continue
-        }
+	for {
+		job, err := client.FetchNextJob()
+		if err != nil {
+			log.Println("[WORKER] error fetching job:", err)
+			time.Sleep(time.Duration(interval) * time.Second)
+			continue
+		}
 
-        if job == nil {
-            log.Println("[WORKER] no jobs, sleeping...")
-            time.Sleep(time.Duration(interval) * time.Second * 3)
-            continue
-        }
+		if job == nil {
+			log.Println("[WORKER] no jobs, sleeping...")
+			time.Sleep(time.Duration(interval) * time.Second * 3)
+			continue
+		}
 
-        log.Printf("[WORKER] Job received: id=%s type=%s\n", job.ID, job.Type)
+		log.Printf("[WORKER] Job received: id=%s type=%s\n", job.ID, job.Type)
 
-        stopHB := make(chan struct{})
-        go StartHeartbeat(client, job.ID, workerID, stopHB)
+		stopHB := make(chan struct{})
+		go StartHeartbeat(client, job.ID, workerID, stopHB)
 
-        result, err := func() (map[string]any, error) {
-    defer close(stopHB)
-    return ProcessJob(job, client)
-}()
+		result, err := func() (map[string]any, error) {
+			defer close(stopHB)
+			return ProcessJob(job, client)
+		}()
 
-if err != nil {
-    client.FailJob(job.ID, err)
-} else {
-    client.CompleteJob(job.ID, result, workerID)
-}
-    }
+		if err != nil {
+			client.FailJob(job.ID, err)
+		} else {
+			client.CompleteJob(job.ID, result, workerID)
+		}
+	}
 }
--- worker/video_processor.go
+++ worker/video_processor.go
@@ -5,28 +5,28 @@ import (
 	"log"
 )
 
-func ProcessVideoClip(job *Job, client *BackendClient) error {
+func ProcessVideoClip(job *Job, client *BackendClient) (map[string]any, error) {
 	log.Println("[VIDEO] job start", job.ID)
 
 	client.UpdateProgress(job.ID, 5)
 
 	input, err := video.DownloadYoutube(job.Payload.YoutubeVideoID)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	client.UpdateProgress(job.ID, 30)
 
 	output, err := video.CutVideo(input, job.Payload.Start, job.Payload.End)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	client.UpdateProgress(job.ID, 80)
 
 	url, err := video.UploadResult(output, client.baseURL)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	client.UpdateProgress(job.ID, 100)
